255b8def324e458f6049c568f0501f72c81fa5fc
Safianu <safianusani2004@gmail.com>
2023-09-01 01:50:37 +0100

    About Me

--- app/src/main/java/com/safianu/aboutme/MainActivity.java
+++ app/src/main/java/com/safianu/aboutme/MainActivity.java
@@ -2,11 +2,16 @@ package com.safianu.aboutme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout enterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
             window.setNavigationBarColor(this.getResources().getColor(R.color.black));
             window.setStatusBarColor(this.getResources().getColor(R.color.black));
         }
+
+        enterButton = findViewById(R.id.enterBtn);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
